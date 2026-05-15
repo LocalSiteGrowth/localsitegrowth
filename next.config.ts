d374bed3835bef1e1421416e30a0a09932832916
Local Site Growth <info@localsitegrowth.com>
2026-05-15 16:41:23 +1200

    Configure remotePatterns for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.localsitegrowth.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'landingsite-app-public.s3.us-east-2.amazonaws.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
